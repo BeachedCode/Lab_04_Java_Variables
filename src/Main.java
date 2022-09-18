faad7ea24b594118e3a4c603717384b305b8b8a9
Ethan Beach <Beached@mail.uc.edu>
2022-09-18 18:50:37 -0400

    Part 2 of Lab 4

--- src/Main.java
+++ src/Main.java
@@ -13,5 +13,18 @@ public class Main
         System.out.println("The difference using ints of " + OperandA + " and " + OperandB + " is " + Difference);
         System.out.println("The product using ints of " + OperandA + " and " + OperandB + " is " + Product);
         System.out.println("The quotient using ints of " + OperandA + " and " + OperandB + " is " + Quotient);
+
+        // Next Part ------------------------------------------------------------------------------------------------
+
+        int kidsFam = 0;
+        boolean isRaining = false;
+        double gasPrice = 4.39;
+        int favNum = 6;
+        double shoeSize = 9;
+        String birthMonth = "August";
+        String fullName = "Ethan Beach";
+        double myLunchCost = 12.50;
+        
+        System.out.println("The cost of my lunch is: "  + myLunchCost);
     }
 }
