9d45eb3b828ade8772a69540d137e64af8b3b480
Ethan Beach <Beached@mail.uc.edu>
2022-09-18 18:46:06 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double OperandA = 10;
+        double OperandB = 5;
+        double Sum = OperandA + OperandB;
+        double Product = OperandA * OperandB;
+        double Difference = OperandA - OperandB;
+        double Quotient = OperandA / OperandB;
+
+        System.out.println("The sum using ints of " + OperandA + " and " + OperandB + " is " + Sum);
+        System.out.println("The difference using ints of " + OperandA + " and " + OperandB + " is " + Difference);
+        System.out.println("The product using ints of " + OperandA + " and " + OperandB + " is " + Product);
+        System.out.println("The quotient using ints of " + OperandA + " and " + OperandB + " is " + Quotient);
+    }
+}
